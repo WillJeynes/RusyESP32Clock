e96143cbbf147d97939903597751a094a405fa28
WillJeynes <william.jeynes@hotmail.com>
2025-04-19 19:35:37 +0100

    Starting on error handling

--- src/configuration/setup_display.rs
+++ src/configuration/setup_display.rs
@@ -86,3 +86,13 @@ impl core::fmt::Display for SimpleError {
 
 #[cfg(feature = "std")]
 impl std::error::Error for SimpleError {}
+
+pub trait ContextExt<T> {
+    fn with_context(self) -> Result<T, SimpleError>;
+}
+
+impl<T, E: std::fmt::Display> ContextExt<T> for Result<T, E> {
+    fn with_context(self) -> Result<T, SimpleError> {
+        self.map_err(|e| SimpleError::new("Unhandled Ex"))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ use esp_idf_svc::http::client::EspHttpConnection;
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
 use esp_idf_svc::wifi::{BlockingWifi, EspWifi};
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use crate::configuration::setup_display::{setup_display, DisplayDriver};
+use crate::configuration::setup_display::{setup_display, DisplayDriver, SimpleError};
 use crate::configuration::setup_wifi::{connect_wifi, get_request, get_request_raw};
 use embedded_svc::{
     http::{client::Client as HttpClient},
@@ -51,7 +51,7 @@ const DEBUG: &str = std::env!("DEBUG");
 
 const LOCATION: &str = std::env!("LOCATION");
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> anyhow::Result<()> {
     let is_debug: bool = DEBUG == "TRUE";
 
     // It is necessary to call this function once. Otherwise, some patches to the runtime
@@ -63,18 +63,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     log::info!("Hello, world!");
 
     //Init Peripherals
-    let peripherals = Peripherals::take().unwrap();
+    let peripherals = Peripherals::take()?;
 
     //Display
     let mut display = setup_display(peripherals.pins, peripherals.spi2)?;
 
     let cls_pixels = AlwaysSame {value: if (is_debug) { Rgb565::BLUE } else { Rgb565::BLACK} };
     display.set_pixels(0, 0, 500, 250, cls_pixels.into_iter().take(500*250) )
-        .map_err(|_| Box::<dyn Error>::from("draw world"))?;
+        .map_err(|e| SimpleError::new("Set Pixels"))?;
 
     let cls_pixels = AlwaysSame {value: if (is_debug) { Rgb565::RED } else { Rgb565::BLACK } };
     display.set_pixels( 0,250,500,350, cls_pixels.into_iter().take(500 * 100) )
-        .map_err(|_| Box::<dyn Error>::from("draw world"))?;
+        .map_err(|e| SimpleError::new("Set Pixels"))?;
 
     log::info!("Cleared Display");
 
@@ -86,13 +86,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn draw_err(err : Box<dyn Error>, display : &mut DisplayDriver) {
+fn draw_err(err : anyhow::Error, display : &mut DisplayDriver) {
     let error_style = MonoTextStyle::new(&FONT_10X20, Rgb565::RED);
-    Text::new(&format!("Unhandled Exception Occured: {}", err), Point::new(10, 50), error_style)
+    Text::new(&format!("Ex: {}", err), Point::new(10, 50), error_style)
         .draw(display)
         .unwrap();
 }
-fn run(modem: Modem, mut display: &mut DisplayDriver) -> Result<(), Box<dyn Error>> {
+fn run(modem: Modem, mut display: &mut DisplayDriver) -> anyhow::Result<()> {
     let is_debug: bool = DEBUG == "TRUE";
 
     //Wi-Fi
@@ -104,8 +104,19 @@ fn run(modem: Modem, mut display: &mut DisplayDriver) -> Result<(), Box<dyn Erro
         sys_loop,
     )?;
 
-    connect_wifi(&mut wifi)?;
-    //TODO: add retry logic here
+    for attempt in 1..=3 {
+        match connect_wifi(&mut wifi) {
+            Ok(_) => {
+                log::info!("Connected to Wi-Fi on attempt {}", attempt);
+            }
+            Err(e) => {
+                log::error!("Failed to connect (attempt {}): {}", attempt, e);
+                if attempt < 3 {
+                    sleep(Duration::from_secs(2));
+                }
+            }
+        }
+    }
 
     let mut client = HttpClient::wrap(EspHttpConnection::new(&Default::default())?);
     log::info!("Connected WiFi");
@@ -116,8 +127,6 @@ fn run(modem: Modem, mut display: &mut DisplayDriver) -> Result<(), Box<dyn Erro
     let request_clock = request_string.parse::<i64>().unwrap();
     let request_millis = unsafe {esp_timer_get_time()} / 1000;
     log::info!("Got Time Request {} at {}",request_string,  request_millis);
-
-    //display.clear(Rgb565::BLUE).map_err(|_| Box::<dyn Error>::from("draw world"))?;
 
     let mut current_date = String::from("");
     let mut current_time = String::from("aaaaaa");
@@ -167,17 +176,17 @@ fn run(modem: Modem, mut display: &mut DisplayDriver) -> Result<(), Box<dyn Erro
             for number in 0..time_string.len() {
                 if let (Some(c1), Some(c2)) = (time_string.chars().nth(number), current_time.chars().nth(number)) {
                     if c1 != c2 {
-                        let index: usize = c1.to_string().parse().unwrap();
+                        let index: usize = c1.to_string().parse()?;
 
-                        let isSmall = number > 3;
+                        let is_small = number > 3;
 
-                        let offset_x : i32 = if !isSmall {
+                        let offset_x : i32 = if !is_small {
                             10 + (number as i32 * ((def_scale * 15) + 20))
                         }
                         else {
                             10 + (4 * ((def_scale * 15) + 20)) + ((number as i32- 4) * ((small_scale * 15) + 5))
                         };
-                        let scale : i32 = if isSmall { small_scale} else {def_scale};
+                        let scale : i32 = if is_small { small_scale} else {def_scale};
 
                         let pixels = font_bmps[index].pixels();
 
@@ -189,7 +198,7 @@ fn run(modem: Modem, mut display: &mut DisplayDriver) -> Result<(), Box<dyn Erro
                                 (offset_x + (position.x + 1) * scale) as u16,
                                 (offset_y + (position.y + 1) * scale) as u16,
                                 display_pixels.into_iter().take((scale * (scale + 1)) as usize))
-                                .map_err(|_| Box::<dyn Error>::from("draw world"))?;
+                                .map_err(|e| SimpleError::new("Set Pixels"))?;;
                         }
                     }
                 }
@@ -200,26 +209,26 @@ fn run(modem: Modem, mut display: &mut DisplayDriver) -> Result<(), Box<dyn Erro
         else if (date_string != current_date) {
             let cls_pixels = AlwaysSame {value: if (is_debug) { Rgb565::GREEN } else { Rgb565::BLACK } };
             display.set_pixels(10, 185, 450, 205, cls_pixels.into_iter().take(550*20) )
-                .map_err(|_| Box::<dyn Error>::from("draw world"))?;
+                .map_err(|e| SimpleError::new("Set Pixels"))?;
 
             let date_style = MonoTextStyle::new(&FONT_10X20, Rgb565::WHITE);
             Text::new(&date_string, Point::new(10, 200), date_style)
                 .draw(display)
-                .map_err(|_| Box::<dyn Error>::from("draw world"))?;
+                .map_err(|e| SimpleError::new("Set Pixels"))?;
 
             current_date = date_string;
         }
         else if ((current_millis - last_updated_at_a) > 50000) {
-            let bmp_data = get_request_raw(&mut client, format!("{}/Time/Image/0", BASEURL)).map_err(|_| Box::<dyn Error>::from("draw world"))?;
+            let bmp_data = get_request_raw(&mut client, format!("{}/Time/Image/0", BASEURL)).map_err(|e| SimpleError::new("Set Pixels"))?;;
             let bmp = Bmp::<Rgb565>::from_slice(&bmp_data).unwrap();
-            Image::new(&bmp, Point::new( 10, 220)).draw(display).map_err(|_| Box::<dyn Error>::from("draw world"))?;
+            Image::new(&bmp, Point::new( 10, 220)).draw(display).map_err(|e| SimpleError::new("Set Pixels"))?;;
 
             last_updated_at_a = current_millis;
         }
         else if ((current_millis - last_updated_at_b) > 50000) {
-            let bmp_data = get_request_raw(&mut client, format!("{}/Time/Image/1", BASEURL)).map_err(|_| Box::<dyn Error>::from("draw world"))?;
+            let bmp_data = get_request_raw(&mut client, format!("{}/Time/Image/1", BASEURL)).map_err(|e| SimpleError::new("Set Pixels"))?;;
             let bmp = Bmp::<Rgb565>::from_slice(&bmp_data).unwrap();
-            Image::new(&bmp, Point::new( 250, 220)).draw(display).map_err(|_| Box::<dyn Error>::from("draw world"))?;
+            Image::new(&bmp, Point::new( 250, 220)).draw(display).map_err(|e| SimpleError::new("Set Pixels"))?;;
 
             last_updated_at_b = current_millis;
         }
